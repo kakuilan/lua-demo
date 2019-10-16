8c280f5a496c9ec4f3022099c4a7fb0ac8e7ad98
kakuilan <kakuilan@163.com>
2019-10-16 23:01:10 +0800

    add 函数返回2个值的最大值

---
+++ t014.lua
@@ -0,0 +1,14 @@
+-- 函数返回2个值的最大值
+
+function max(num1, num2)
+  if (num1 > num2) then
+    result = num1;
+  else
+    result = num2;
+  end
+  return result;  
+end
+
+-- 调用函数
+print('两值比较最大值为:', max(10, 4))
+print('两值比较最大值为:', max(4, 6))
