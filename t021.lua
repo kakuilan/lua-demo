d0e478448080f63b6370df8e539eb008e3a1d3d5
kakuilan <kakuilan@163.com>
2019-10-17 10:46:22 +0800

    add 算术运算符

---
+++ t021.lua
@@ -0,0 +1,26 @@
+-- 算术运算符
+
+a = 21
+b = 10
+
+c = a+b
+print('Line 1: c的值为:', c)
+
+c = a - b
+print('Line 2: c的值为:', c)
+
+c = a * b
+print('Line 3: c的值为:', c)
+
+c = a / b
+print('Line 4: c的值为:', c)
+
+c = a % b
+print('Line 5: c的值为:', c)
+
+c = a ^ 2
+print('Line 6: c的值为:', c)
+
+c = -a 
+print('Line 7: c的值为:', c)
+
